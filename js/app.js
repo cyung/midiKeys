236c323346c0ed3cc5f308db70f76e6e4a441a32
Chris Yung <cyung@bu.edu>
2015-04-23 04:31:49 -0400

    black keys displayed properly

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /**
-*  Module
-*
-* Description
-*/
+ *  Module
+ *
+ * Description
+ */
 angular.module('app', []);
 
 angular.module('app')
@@ -11,7 +11,26 @@ angular.module('app')
 		self.piano_back = 'img/background.png';
 		self.white_key = 'img/midi_white_up.png';
 		self.black_key = 'img/midi_black_up.png';
-		this.range = function(num) {
+
+		self.keys = [];
+		var keyGroupOf3 = true;
+
+		self.keys.push({black: true}); // first key is shown
+		var i = 1;
+		while (i < 54) {
+			self.keys.push({black: false});
+			// alwasy followed by at least two
+			self.keys.push({black: true});
+			self.keys.push({black: true});
+			if (keyGroupOf3){
+				self.keys.push({black: true});
+				i += 4;
+			} else {
+				i += 3;
+			}
+			keyGroupOf3 = !keyGroupOf3;
+		}
+		self.range = function(num) {
 			return new Array(num);
 		};
 	});

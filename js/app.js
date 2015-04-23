9bd3f5c7ce04d229ace2dfacc7ee4db981ccb569
Chris Yung <cyung@bu.edu>
2015-04-23 01:38:27 -0400

    added piano keys

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ angular.module('app', []);
 angular.module('app')
 	.controller('DrawCtrl', function() {
 		var self = this;
-		self.img = 'img/background.png';
-
+		self.piano_back = 'img/background.png';
+		self.white_key = 'img/midi_white_up.png';
+		self.black_key = 'img/midi_black_up.png';
 		this.range = function(num) {
 			return new Array(num);
 		};

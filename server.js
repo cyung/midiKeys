30d98197a1a1556ce7b19ac090ca62eb5100b49e
Hack Reactor Students <accounts@hackreactor.com>
2015-09-17 17:26:01 -0700

    basic server implementation

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var http = require('http');
+// var passport = require('passport');
+
+var port = 3000;
+
+var app = express();
+app.use(express.static(__dirname + '/client'));
+
+
+app.get('/practice', function(req, res, next){
+  res.sendFile(__dirname + '/client/practice.html');
+});
+
+app.get('/play', function(req, res, next){
+  res.sendFile(__dirname + '/client/play.html');
+});
+
+app.get('/contact', function(req, res, next){
+  res.sendFile(__dirname + '/client/contact.html');
+});
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Chris\' awesome app listening at http://%s:%s', host, port);
+});
